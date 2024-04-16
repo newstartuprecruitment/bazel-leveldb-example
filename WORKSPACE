679ffda516e20ee5db273050e476b85dce84df27
arun <arunmoezhi@gmail.com>
2024-04-16 11:15:15 +0000

    add a simple example

--- WORKSPACE
+++ WORKSPACE
@@ -6,6 +6,7 @@ BAZEL_SKYLIB_VERSION = "1.5.0"
 
 http_archive(
     name = "bazel_skylib",
+    sha256 = "cd55a062e763b9349921f0f5db8c3933288dc8ba4f76dd9416aac68acee3cb94",
     urls = [
         "https://github.com/bazelbuild/bazel-skylib/releases/download/{version}/bazel-skylib-{version}.tar.gz".format(version = BAZEL_SKYLIB_VERSION),
     ],
@@ -21,6 +22,7 @@ http_archive(
 http_archive(
     name = "com_github_google_snappy",
     build_file = "//:snappy.BUILD",
+    sha256 = "3dfa02e873ff51a11ee02b9ca391807f0c8ea0529a4924afa645fbf97163f9d4",
     strip_prefix = "snappy-1.1.7",
     urls = ["https://github.com/google/snappy/archive/1.1.7.tar.gz"],
 )
@@ -28,6 +30,7 @@ http_archive(
 http_archive(
     name = "com_github_google_crc32c",
     build_file = "//:crc32c.BUILD",
+    sha256 = "97d404992dcab9ab6be56067a37889b0cba1ea6cabcc2bc3feeb0131ee35a340",
     strip_prefix = "crc32c-21fc8ef30415a635e7351ffa0e5d5367943d4a94",
     urls = ["https://github.com/google/crc32c/archive/21fc8ef30415a635e7351ffa0e5d5367943d4a94.tar.gz"],
 )
--- main.cc
+++ main.cc
@@ -1,12 +1,80 @@
-#include "leveldb/db.h"
 #include <cassert>
 #include <iostream>
-int main()
-{
-    leveldb::DB *db;
+
+#include "leveldb/db.h"
+#include "leveldb/write_batch.h"
+
+using namespace std;
+
+class LocalLevelDb {
+ public:
+  LocalLevelDb() {
     leveldb::Options options;
     options.create_if_missing = true;
     leveldb::Status status = leveldb::DB::Open(options, "/tmp/testdb", &db);
     assert(status.ok());
-    return 0;
+  }
+
+  string get(string k) {
+    leveldb::ReadOptions readOptions;
+    leveldb::Slice key = k;
+    string value;
+    db->Get(readOptions, key, &value);
+    return value;
+  }
+
+  void getBatch(string keyPrefix) {
+    int l = keyPrefix.size();
+    string limit(1, keyPrefix.at(l - 1) + 1);
+    limit = keyPrefix.substr(0, l - 1) + limit;
+    leveldb::Iterator *it = db->NewIterator(leveldb::ReadOptions());
+    for (it->Seek(keyPrefix); it->Valid() && it->key().ToString() < limit;
+         it->Next()) {
+      std::cout << it->key().ToString() << " " << it->value().ToString()
+                << "\n";
+    }
+  }
+
+  void getAll() {
+    leveldb::Iterator *it = db->NewIterator(leveldb::ReadOptions());
+    for (it->SeekToFirst(); it->Valid(); it->Next()) {
+      std::cout << it->key().ToString() << " " << it->value().ToString()
+                << "\n";
+    }
+  }
+
+  void add(string k, string v) {
+    leveldb::WriteOptions writeOptions;
+    leveldb::Slice key = k;
+    leveldb::Slice value = v;
+    db->Put(writeOptions, key, value);
+  }
+
+  void deleteBatch(string keyPrefix) {
+    int l = keyPrefix.size();
+    string limit(1, keyPrefix.at(l - 1) + 1);
+    limit = keyPrefix.substr(0, l - 1) + limit;
+    leveldb::WriteBatch batch;
+    leveldb::Iterator *it = db->NewIterator(leveldb::ReadOptions());
+    for (it->Seek(keyPrefix); it->Valid() && it->key().ToString() < limit;
+         it->Next()) {
+      batch.Delete(it->key());
+    }
+    db->Write(leveldb::WriteOptions(), &batch);
+  }
+
+ private:
+  leveldb::DB *db;
+};
+
+int main() {
+  LocalLevelDb db;
+  db.add("k1-1", "v1");
+  db.add("k1-2", "v2");
+  db.add("k2-1", "v1");
+  db.getBatch("k1");
+  db.deleteBatch("k1");
+  db.getAll();
+
+  return 0;
 }
